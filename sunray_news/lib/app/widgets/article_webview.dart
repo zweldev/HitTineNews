bca350ebc164aff3a0d18fb60a88f82f04e058f1
zweldev <zweldev@gmail.com>
2023-05-19 15:10:07 +0630

    webview update

--- sunray_news/lib/app/widgets/article_webview.dart
+++ sunray_news/lib/app/widgets/article_webview.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
@@ -17,20 +19,24 @@ class _ArticleWebViewState extends State<ArticleWebView> {
   String urlToContent = '';
 
   InAppWebViewController? webViewController;
+  double progress = 0;
 
   PullToRefreshController? pullToRefreshController;
   GlobalKey webViewKey = GlobalKey();
+
   @override
   void initState() {
     super.initState();
     //
     webViewController?.loadUrl(
         urlRequest: URLRequest(url: Uri.parse(urlToContent)));
+
+    //
     pullToRefreshController = kIsWeb
         ? null
         : PullToRefreshController(
             options: PullToRefreshOptions(
-              color: Colors.red,
+              color: lightTheme.colorScheme.secondary,
             ),
             onRefresh: () async {
               if (defaultTargetPlatform == TargetPlatform.android) {
@@ -42,68 +48,125 @@ class _ArticleWebViewState extends State<ArticleWebView> {
               }
             },
           );
+
+    //
   }
 
   @override
   Widget build(BuildContext context) {
     urlToContent = ModalRoute.of(context)!.settings.arguments.toString();
-    return SafeArea(
-      child: Column(
-        children: [
-          Expanded(
-              flex: 3,
-              child: InAppWebView(
-                key: webViewKey,
-                initialUrlRequest: URLRequest(url: Uri.parse(urlToContent)),
-                pullToRefreshController: pullToRefreshController,
-                onWebViewCreated: (controller) {
-                  webViewController = controller;
-                },
-                onLoadStart: (controller, url) {
-                  setState(() {
-                    urlToContent = url.toString();
-                  });
-                },
-                onLoadStop: (controller, url) async {
-                  pullToRefreshController?.endRefreshing();
-                  setState(() {
-                    urlToContent = url.toString();
-                  });
-                },
-                onLoadError: (controller, url, code, message) {
-                  pullToRefreshController?.endRefreshing();
-                },
-              )),
-          Container(
-            decoration: BoxDecoration(color: lightTheme.colorScheme.secondary),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      icon: Icon(
-                        Icons.arrow_back,
-                        color: Colors.grey.shade400,
-                      )),
-                  IconButton(
-                      onPressed: () {
-                        PullToRefreshController(
-                            onRefresh: () => webViewController?.reload());
-                      },
-                      icon: Icon(
-                        Icons.refresh,
-                        color: Colors.grey.shade400,
-                      ))
-                ],
+    return Scaffold(
+      // backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                flex: 3,
+                child: InAppWebView(
+                  key: webViewKey,
+                  initialUrlRequest: URLRequest(url: Uri.parse(urlToContent)),
+                  pullToRefreshController: pullToRefreshController,
+                  // initialOptions: options,
+                  onWebViewCreated: (controller) {
+                    webViewController = controller;
+                  },
+                  onLoadStart: (controller, url) {
+                    setState(() {
+                      urlToContent = url.toString();
+                    });
+                  },
+                  onProgressChanged: (con, progress) {
+                    if (progress == 100) {
+                      pullToRefreshController?.endRefreshing();
+                    }
+                    setState(() {
+                      this.progress = progress / 100;
+                    });
+                  },
+                  onLoadStop: (controller, url) async {
+                    pullToRefreshController?.endRefreshing();
+                    setState(() {
+                      urlToContent = url.toString();
+                    });
+                  },
+                  onLoadError: (controller, url, code, message) {
+                    pullToRefreshController?.endRefreshing();
+                  },
+                )),
+            progress < 1.0
+                ? SizedBox(
+                    height: 3,
+                    child: LinearProgressIndicator(
+                      value: progress,
+                      color: lightTheme.colorScheme.secondary,
+                    ),
+                  )
+                : SizedBox(),
+            Container(
+              decoration:
+                  BoxDecoration(color: lightTheme.colorScheme.secondary),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: Icon(
+                          Icons.arrow_back,
+                          color: Colors.grey.shade400,
+                        )),
+                    IconButton(
+                        onPressed: () {
+                          webViewController?.reload();
+                        },
+                        icon: Icon(
+                          Icons.refresh,
+                          color: Colors.grey.shade400,
+                        ))
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
+
+/** COOKIES STUFFS
+ * 
+ * 
+ * // get the CookieManager instance
+CookieManager cookieManager = CookieManager.instance();
+
+// set the expiration date for the cookie in milliseconds
+final expiresDate = DateTime.now().add(Duration(days: 3)).millisecondsSinceEpoch;
+final url = WebUri("https://flutter.dev/");
+
+// set the cookie
+await cookieManager.setCookie(
+  url: url,
+  name: "myCookie",
+  value: "myValue",
+  expiresDate: expiresDate,
+  isSecure: true,
+);
+
+// get cookies
+List<Cookie> cookies = await cookieManager.getCookies(url: url);
+
+// get a cookie
+Cookie? cookie = await cookieManager.getCookie(url: url, name: "myCookie");
+
+// delete a cookie
+await cookieManager.deleteCookie(url: url, name: "myCookie");
+
+// delete cookies
+await cookieManager.deleteCookies(url: url, domain: ".flutter.dev");
+ * 
+ */
