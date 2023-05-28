a770dbdde418a7e17ed1c81fb434ee2da6996dfa
zweldev <zweldev@gmail.com>
2023-05-28 23:16:12 +0630

    connectivity_update

--- sunray_news/lib/app/modules/main/views/main_view.dart
+++ sunray_news/lib/app/modules/main/views/main_view.dart
@@ -1,26 +1,74 @@
+import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:splash_view/splash_view.dart';
 import '../../auth/cubit/auth_cubit.dart';
 import '../../bookmark/view/bookmark_view.dart';
 import '../../home/view/home_view.dart';
 import '../../search/view/search_view.dart';
 import '../widgets/bottom_nav_bar.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 PageController _pageController = PageController();
 
-class MainView extends StatelessWidget {
+class MainView extends StatefulWidget {
   MainView({super.key});
-
   static String route = "/main_view";
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  final Connectivity _connectivity = Connectivity();
+
+  ConnectivityResult _connectionStatus = ConnectivityResult.none;
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
 
   List<Widget> pages = [
     HomeView(),
     SearchView(),
     BookMarkView(),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(
+      Duration(seconds: 5),
+      (w) {
+        _connectivity.onConnectivityChanged.listen((event) {
+          updateConnectionStatus(event);
+         
+        });
+         log('connectivity status ${_connectionStatus}');
+        // showToast();
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _connectivitySubscription.cancel();
+  }
+
+  Future<void> updateConnectionStatus(ConnectivityResult result) async {
+    setState(() {
+      _connectionStatus = result;
+    });
+  }
+
+  void showToast() async {
+    _connectivity.onConnectivityChanged.listen((event) {
+      event == ConnectivityResult.none
+          ? Fluttertoast.showToast(msg: 'NO INTERNET')
+          : null;
+    });
+  }
 
   int currentIndex = 3;
 
